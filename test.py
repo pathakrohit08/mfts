8055ab4d41579370f730df08028aae6948a6ec9d
Rohit Pathak <pathakrohit08@gmail.com>
2022-03-16 15:22:15 -0400

    first push

--- test.py
+++ test.py
@@ -32,4 +32,5 @@ def send_email(subject, text, file_name=''):
         print("Exception occured "+str(e))
 
 if __name__=='__main__':
+    print("1")
     send_email("commit received","Action was run")
